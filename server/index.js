bf881dcfe1fcfcfb3af91ba546056d6d3def3593
Piyush Pandey <piyush2003pandey@gmail.com>
2024-05-06 08:21:28 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:3000",
+    origin: "https://study-notion-six-zeta.vercel.app/",
     credentials: true,
 }))
 app.use(fileUpload({
