5f7eee5ab64c752cb2125d4b5fd446dbe144960e
Piyush Pandey <piyush2003pandey@gmail.com>
2024-07-16 16:55:50 +0530

    review slider responsiveness update

--- src/components/common/ReviewSlider.jsx
+++ src/components/common/ReviewSlider.jsx
@@ -1,94 +1,98 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {Swiper, SwiperSlide} from "swiper/react"
 import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/pagination"
-import { Autoplay,FreeMode,Navigation, Pagination}  from 'swiper/modules'
-import ReactStars from "react-rating-stars-component";
-import { apiConnector } from '../../services/apiconnector'
-import { ratingsEndpoints } from '../../services/apis'
-import { FaStar } from 'react-icons/fa'
+import "swiper/css/navigation";
+import { Autoplay,Mousewheel, Keyboard}  from 'swiper'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect, useState } from 'react';
+import { ratingsEndpoints } from '../../../services/apis'
+import { apiConnector } from '../../../services/apiConnector'
+import RatingStars from '../../common/RatingStars'
 
-const ReviewSlider = () => {
 
-    const [reviews,setReviews] = useState([])
-    const truncateWords = 15
+const RatingSlider = () => {
+    const [Reviews, setReviews] = useState([]);
+    const [Loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const fetchAllReviews = async() => {
-            const {data} = await apiConnector("GET",ratingsEndpoints.REVIEWS_DETAILS_API)
-            if(data?.success){
-                setReviews(data?.data)
+        const getReviews = async () => {
+            setLoading(true);
+            try {
+                const res = await apiConnector("GET",ratingsEndpoints.REVIEWS_DETAILS_API);
+                setReviews(res.data.data);
+                console.log("LOGGING REVIEWS",res);
+            } catch (error) {
+                console.log("LOGGING Review ERROR",error);
+            } finally {
+                setLoading(false);
             }
         }
-        fetchAllReviews()
-    },[])
-
+        getReviews();
+    }, [])
   return (
-    <div className='text-white w-full max-w-maxContent'>
-        <div className="my-[50px] h-[184px] max-w-maxContentTab lg:max-w-maxContent">
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={24}
-                loop={true}
-                freeMode={true}
-                autoplay={{
-                    delay:2500,
-                }}
-                modules={[FreeMode,Pagination,Autoplay]}
-                className='w-full'
-            >
-                {
-                    reviews.map((review,index) => (
-                        <SwiperSlide key={index}>
-                            <div className="flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25">
-                            <div className="flex items-center gap-4">
-                                <img
-                                src={
-                                    review?.user?.image
-                                    ? review?.user?.image
-                                    : `https://api.dicebear.com/5.x/initials/svg?seed=${review?.user?.firstName} ${review?.user?.lastName}`
-                                }
-                                alt=""
-                                className="h-9 w-9 rounded-full object-cover"
-                                />
-                                <div className="flex flex-col">
-                                <h1 className="font-semibold text-richblack-5">{`${review?.user?.firstName} ${review?.user?.lastName}`}</h1>
-                                <h2 className="text-[12px] font-medium text-richblack-500">
-                                    {review?.course?.courseName}
-                                </h2>
+    <div>
+        <Swiper
+                 mousewheel={
+                      {
+                          enabled: true,
+                          forceToAxis: true,
+                      } 
+                 }
+                 keyboard={
+                      {
+                          enabled: true,
+                          onlyInViewport: true,
+                      }
+                 }
+                 allowSlidePrev={true}
+                    slidesPerView={1}
+                    loop={true}
+                    spaceBetween={20}
+                    pagination={false}
+                    modules={[Mousewheel,Keyboard, Autoplay]}
+                    className="mySwiper md:pt-5"
+                    autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                    }}
+                    style={{
+                        "--swiper-navigation-size": "20px",
+                      }}
+                    freeMode={false}
+                    rewind={false}
+                    centeredSlides={true}
+                    navigation={false}
+                    breakpoints={{
+                        300:{slidesPerView:1.1,spaceBetween:10,},
+                        640:{slidesPerView:2.2,},
+                        1024:{slidesPerView:3.1,}
+                    }}
+                    
+                   
+                >
+                    {
+                        Reviews?.map((review, index)=> (
+                            <SwiperSlide key={index}>
+                                <div className='flex flex-col gap-3 min-h-[150px] bg-richblack-800 p-3 text-[14px] text-richblack-25'>
+                                    <div className='flex items-center gap-4'>
+                                        <img src={review?.user?.image} alt="user" className='h-9 w-9 rounded-full object-cover' />
+                                        <div className='flex flex-col'>
+                                            <h3 className='font-semibold text-richblack-5'>{review?.user?.firstName} {review?.user.lastName}</h3>
+                                            <p className='text-[12px] font-medium text-richblack-500'>{review?.course?.courseName}</p>
+                                        </div>
+                                    </div>
+                                    <div className='font-medium text-richblack-25'>{review?.review.slice(0,70)}...</div>
+                                    <RatingStars Review_Count={review?.rating} />
                                 </div>
-                            </div>
-                            <p className="font-medium text-richblack-25">
-                                {review?.review.split(" ").length > truncateWords
-                                ? `${review?.review
-                                    .split(" ")
-                                    .slice(0, truncateWords)
-                                    .join(" ")} ...`
-                                : `${review?.review}`}
-                            </p>
-                            <div className="flex items-center gap-2 ">
-                                <h3 className="font-semibold text-yellow-100">
-                                {review.rating.toFixed(1)}
-                                </h3>
-                                <ReactStars
-                                count={5}
-                                value={review.rating}
-                                size={20}
-                                edit={false}
-                                activeColor="#ffd700"
-                                emptyIcon={<FaStar />}
-                                fullIcon={<FaStar />}
-                                />
-                            </div>
-                            </div>
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
-        </div>
+                            </SwiperSlide>
+                        ))
+                    }   
+                </Swiper>
     </div>
   )
 }
 
-export default ReviewSlider
+export default RatingSlider
