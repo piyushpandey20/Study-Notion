3839ddd0709de1dde913e56e0289ef8cf6f62c27
Piyush Pandey <piyush2003pandey@gmail.com>
2024-08-14 13:32:37 +0530

    updating for review slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -196,14 +196,12 @@ const Home = () => {
 
 
       {/*Section 3 */}
-      <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
-
-            <InstructorSection />
-
-            <h2 className='text-center text-4xl font-semobold mt-10'>review from Other Learners</h2>
-            {/* Review Slider here */}
-            <ReviewSlider/>
-            
+      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+        <InstructorSection />
+        <h1 className="text-center text-4xl font-semibold mt-8">
+          Reviews from other learners
+        </h1>
+        <ReviewSlider />
       </div>
 
 
